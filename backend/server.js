d2709117d3ab08d049e1a65a13a4b21bb7fbf48a
sryagit <158165193+sryagit@users.noreply.github.com>
2025-09-18 21:40:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://surajtodo:surajtodo123@clusteraaws.nt3dbmw.mongodb.net/?retryWrites=true&w=majority&appName=ClusteraAWS', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+mongoose.connect('mongodb+srv://surajtodo:surajtodo123@clusteraaws.nt3dbmw.mongodb.net/?retryWrites=true&w=majority&appName=ClusteraAWS')
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
 
 // Task Schema
 const taskSchema = new mongoose.Schema({
@@ -39,5 +36,6 @@ app.delete('/tasks/:id', async (req, res) => {
     res.json({ message: 'Task deleted' });
 });
 
-// Start server
-app.listen(5000, () => console.log('Server running on port 5000'));
+// ✅ Use Render’s dynamic port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
